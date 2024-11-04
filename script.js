35e66eb925034db253b58fcbdd9ad8ef245cce3a
I MADE ARYA DHARMA WHASUDEWA <141533538+kelanabersamaarya@users.noreply.github.com>
2024-11-04 23:22:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuIcon = document.getElementById("menu-icon");
+    const menuList = document.getElementById("menu-list");
+
+    menuIcon.addEventListener("click", () => {
+        menuList.classList.toggle("hidden"); 
+    });
+});
+
